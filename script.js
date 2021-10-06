1fec05b22994633ebdd4bf3004e1225dbdb4480d
Ashik-Himel <85483328+Ashik-Himel@users.noreply.github.com>
2021-10-06 22:44:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('menu-bar').onclick = function() {
+    document.getElementById('navigation').classList.toggle('show-menu')
+}
+document.getElementById('search-icon').onclick = function() {
+    document.getElementById('search-bar').classList.toggle('show-search')
+}
